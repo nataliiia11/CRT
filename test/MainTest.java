610a7ee657cc281970688d96632a3e70c5a84e04
nataliiia11 <borshiq111@gmail.com>
2022-11-06 21:30:16 +0100

    "added unit tests "

--- test/MainTest.java
+++ test/MainTest.java
@@ -10,12 +10,32 @@ public class MainTest {
                 Assert.assertEquals(4, Main.multiplicativeInverse(3, 11));
                 Assert.assertEquals(12, Main.multiplicativeInverse(10, 17));
                 Assert.assertEquals(6, Main.multiplicativeInverse(2, 11));
-
+                Assert.assertEquals(8, Main.multiplicativeInverse(5, 13));
+                Assert.assertEquals(51, Main.multiplicativeInverse(103, 101));
+                Assert.assertEquals(46, Main.multiplicativeInverse(11, 101));
+                Assert.assertEquals(74, Main.multiplicativeInverse(51, 77));
         }
 
                 @Test
         public void testComputeY() {
-                Assert.assertEquals(BigInteger.valueOf(1968), Main.computeY(BigInteger.valueOf(3000), BigInteger.valueOf(500), 103,101));
+              Assert.assertEquals(BigInteger.valueOf(1968), Main.computeY(BigInteger.valueOf(500), BigInteger.valueOf(3000), 103,101));
                 }
 
-        }
+                @Test
+                public void testComputeY2() throws IllegalArgumentException {
+
+                    int num1 = 36;
+                    int num2 = 206;
+                    int prime1 = 103;
+                    int prime2 = 107;
+                    int expectRes = (num1 * num2) % (prime1 * prime2);
+                    if(num1 < prime1 * prime2 && num2 < prime1*prime2) {
+                        Assert.assertEquals(BigInteger.valueOf(expectRes), Main.computeY(BigInteger.valueOf(num1), BigInteger.valueOf(num2), prime1, prime2));
+                    } else {
+                        throw new IllegalArgumentException("both X and Y should be smaller than p * q");
+                    }
+                }
+
+
+
+}
